ee874424ba0875b991ccea9a4db847cc7c89f044
PJ <pj.reiniger@gmail.com>
2015-01-07 20:25:28 -0500

    Forgot more stuff...

---
+++ SimpleMockWpiLib/src/com/snobot/simulator/gui/GraphicalSensorDisplayPanel.java
@@ -0,0 +1,50 @@
+package com.snobot.simulator.gui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import com.snobot.simulator.SensorActuatorRegistry;
+import com.snobot.simulator.gui.advanced_widgets.DigitalSourceGraphicDisplay;
+import com.snobot.simulator.gui.advanced_widgets.RelayGraphicDisplay;
+import com.snobot.simulator.gui.advanced_widgets.SolenoidGraphicDisplay;
+import com.snobot.simulator.gui.advanced_widgets.SpeedControllerGraphicDisplay;
+
+public class GraphicalSensorDisplayPanel extends JPanel 
+{
+	public GraphicalSensorDisplayPanel()
+	{
+		create();
+	}
+	
+	
+	private SpeedControllerGraphicDisplay mSpeedControllerPanel;
+	private SolenoidGraphicDisplay mSolenoidPanel;
+	private DigitalSourceGraphicDisplay mDigitalSourcePanel;
+	private RelayGraphicDisplay mRelayPanel;
+	
+	public void create()
+	{
+		SensorActuatorRegistry reg = SensorActuatorRegistry.get();
+		
+		mSpeedControllerPanel = new SpeedControllerGraphicDisplay(reg.getSpeedControllers());
+		mSolenoidPanel = new SolenoidGraphicDisplay(reg.getSolenoids());
+		mDigitalSourcePanel = new DigitalSourceGraphicDisplay(reg.getDigitalSources());
+		mRelayPanel = new RelayGraphicDisplay(reg.getRelays());
+
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(mSpeedControllerPanel);
+		add(mSolenoidPanel);
+		add(mDigitalSourcePanel);
+		add(mRelayPanel);
+	}
+
+	public void update() 
+	{
+		SensorActuatorRegistry reg = SensorActuatorRegistry.get();
+
+		mSpeedControllerPanel.update(reg.getSpeedControllers());
+		mSolenoidPanel.update(reg.getSolenoids());
+		mDigitalSourcePanel.update(reg.getDigitalSources());
+		mRelayPanel.update(reg.getRelays());
+	}
+}
