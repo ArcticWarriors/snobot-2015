8066ad7156ed2e524913f7c74f3028c872c6377a
Calvin Do <blazezekrom@gmail.com>
2015-01-16 18:58:45 -0500

    Adding last years smart dashboard extensions

---
+++ DashboardExtensions/SnobotSDExtensions2014/src/com/snobot/sd/SDKeys.java
@@ -0,0 +1,38 @@
+package com.snobot.sd;
+
+public class SDKeys
+{
+    public static final String sTANK_PRESSURE_SD_KEY = "Tank Pressure";
+
+    //Autonomous
+    public static final String sAUTON_MODE_SD_KEY    = "Auton Mode";
+    public static final String sAUTON_COMMANDS_KEY = "Auton Commands";
+    public static final String sAUTON_MODE_NAME_KEY = "Auton Name";
+    public static final String sCURRENT_STEP_KEY = "Current Auton Step";
+    public static final String sFILE_SUCCESS_KEY = "Auton File Success";
+
+    //Camera
+    public static final String GOAL_IN_RANGE = "Goal in range";
+    public static final String GOAL_IS_HOT = "Goal is hot";
+    
+    //Drivetrain
+    public static final String sDISTANCE_X_SD_KEY = "Robot X";
+    public static final String sDISTANCE_Y_SD_KEY = "Robot Y";
+    public static final String sANGLE_KEY = "Angle";
+    public static final String sDRIVE_GEAR_SD_KEY = "Driving Gear";
+    public static final String sIS_CALIBRATING_SD_KEY = "Wheel Calibration Mode";
+    public static final String sWHEEL_TO_CAL_SD_KEY = "Wheel to Calibrate";
+    
+    //Harvester
+    public static final String sHARVESTOR_IN_SD_KEY = "Harvester Frame In";
+    public static final String sHARVESTOR_ROLLER_SPEED_SD_KEY = "Over Roller Motor Speed";
+    public static final String sHARVESTER_ARM_OUT_SD_KEY = "Harvesting Out (Sensor)";
+    public static final String sHARVESTER_ARM_IN_SD_KEY = "Harvesting In (Sensor)";
+    
+    //Shooter
+    public static final String sDEFENSE_STATE_SD_KEY = "Defense State";
+    public static final String sSHOOTING_STATE_SD_KEY = "Shooter Solenoid";
+    public static final String sTOSS_COUNT_SD_KEY = "Toss Counter";
+    public static final String sTOSS_MAX_COUNT_SD_KEY = "Toss Max Counter";
+    
+}
