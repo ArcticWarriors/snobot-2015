b707094ef0761d01c7c2fc97813c9ffa0e556146
Alec Hussak <mr.ahussak@gmail.com>
2015-01-17 14:29:37 -0500

    git is stupid

--- snobot2015/src/com/snobot/claw/SnobotClaw.java
+++ snobot2015/src/com/snobot/claw/SnobotClaw.java
@@ -35,5 +35,40 @@ public class SnobotClaw implements IClaw{
 		// TODO Auto-generated method stub
 		
 	}
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void control() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void rereadPreferences() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateSmartDashboard() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateLog() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- snobot2015/src/com/snobot/stacker/SnobotStacker.java
+++ snobot2015/src/com/snobot/stacker/SnobotStacker.java
@@ -26,4 +26,46 @@ public class SnobotStacker implements IStacker{
 		
 	}
 
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void control() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rereadPreferences() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSmartDashboard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateLog() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
