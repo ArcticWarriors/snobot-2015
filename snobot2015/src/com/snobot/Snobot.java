14480452be493c01492e22224b87836232175d40
Alec Hussak <mr.ahussak@gmail.com>
2015-01-20 19:33:23 -0500

    Snobot class with object initializers.

--- snobot2015/src/com/snobot/Snobot.java
+++ snobot2015/src/com/snobot/Snobot.java
@@ -1,7 +1,15 @@
 
 package com.snobot;
 
+import com.snobot.claw.SnobotClaw;
+import com.snobot.drivetrain.SnobotDriveTrain;
+import com.snobot.joystick.SnobotXBoxDriverJoystick;
+import com.snobot.operatorjoystick.SnobotOperatorJoystick;
+import com.snobot.stacker.SnobotStacker;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,12 +19,28 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Snobot extends IterativeRobot {
+	private SnobotOperatorJoystick mOperatorJoystick;
+	private SnobotStacker mStacker;
+	private SnobotXBoxDriverJoystick mXBoxDriverJoystick;
+	private SnobotClaw mClaw;
+	private SnobotDriveTrain mDriveTrain;
+	
+	private Talon mTalonLeft;
+	private Talon mTalonRight;
+	private Joystick mRawOperatorJoystick;
+	private Joystick mRawDriverJoystick;
+	
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+    	mOperatorJoystick = new SnobotOperatorJoystick(mRawOperatorJoystick);
+    	mXBoxDriverJoystick = new SnobotXBoxDriverJoystick(mRawDriverJoystick);
+    	mStacker = new SnobotStacker(mOperatorJoystick);
+    	mClaw = new SnobotClaw (mOperatorJoystick);
+    	mDriveTrain=new SnobotDriveTrain(mTalonLeft, mTalonRight, mXBoxDriverJoystick);
     }
 
     /**
