5b6365f0ef3f32cc85d0fc3fb46f6d36936903c1
Alec Hussak <mr.ahussak@gmail.com>
2015-01-26 14:04:47 -0500

    Updated SnobotPosition with two new methods, an enum for definable units of measure, and better comments.

--- snobot2015/src/com/snobot/position/SnobotPosition.java
+++ snobot2015/src/com/snobot/position/SnobotPosition.java
@@ -38,39 +38,57 @@ public class SnobotPosition{
 	private Gyro mGyroSensor;
 	
 	/**
-	 * Constructs a SnobotPosition object
-	 * @param aPositionX
-	 * @param aPositionY
-	 * @param aRadianRotation
+	 * Unit of measure to return as the distance traveled
 	 */
-	public SnobotPosition(double aPositionX, double aPositionY, double aRadianRotation, Gyro aGyroSensor) {
+	public enum unitOfMeasure{Feet, Meters, Inches, Centimeters}
+	
+	/**
+	 * Default unit of measure used by updateAll()
+	 */
+	private unitOfMeasure mDefaultMeasure;
+	
+	/**
+	 * Constructs a SnobotPosition object
+	 * @param aPositionX Snobot's initial position on the X-axis
+	 * @param aPositionY Snobot's initial position on the Y-axis
+	 * @param aRadianRotation Snobot's initial orientation
+	 * @param aGryroSensor Gyro object to calculate orientation
+	 * @param aDefaultMeasure Measure that is to be used by default
+	 */
+	public SnobotPosition(double aPositionX, double aPositionY, double aRadianRotation, 
+			Gyro aGyroSensor, unitOfMeasure aDefaultMeasure) {
 		mPositionX=aPositionX;
 		mPositionY=aPositionY;
 		mRadianRotation=aRadianRotation;
 		
 		// TODO Add Gyro declaration to com.snobot.Snobot and port to com.snobot.ConfigurationNames (0-1 only)
 		mGyroSensor=aGyroSensor;
+		
+		mDefaultMeasure=aDefaultMeasure;
 	}
 
 	/**
-	 * Returns Snobot's X-position
-	 * @return
+	 * Calculates Snobot's X-position with the opposite
+	 * side of the field being north
+	 * @return Snobot's X-position
 	 */
 	public double getSnobotX() {
 		return Math.sin(mRadianRotation)*mDistanceTraveled+mPositionX;
 	}
 
 	/**
-	 * Returns Snobot's Y-position
-	 * @return
+	 * Calculates Snobot's Y-position with the opposite
+	 * side of the field being north
+	 * @return Snobot's Y-position
 	 */
 	public double getSnobotY() {
 		return Math.cos(mRadianRotation)*mDistanceTraveled;
 	}
 
 	/**
-	 * Returns the direction Snobot is facing in radians
-	 * @return
+	 * Calculates the direction Snobot is facing
+	 * @return The direction Snobot is facing in radians
+	 * between 2PI and -2PI
 	 */
 	public double getSnobotDirection() {
 		double gyroDegrees=mGyroSensor.getAngle();
@@ -85,17 +103,18 @@ public class SnobotPosition{
 	}
 
 	/**
-	 * Returns the distance traveled by Snobot as the
+	 * Calculates the distance traveled by Snobot as the
 	 * average of the left/right distances
-	 * @return
+	 * @return The distance traveled by Snobot
 	 */
-	public double getDistanceTraveled() {
+	public double getDistanceTraveled(unitOfMeasure aMeasure) {
 		double distanceRight;
 		double distanceLeft;
 		
 		// TODO Code for getting wheel rotations goes here  (encoders needed)
 		// Forward/backward must be considered
-		// TODO distanceLeft/distanceRight are also declared here
+		// TODO Add different sets of calculations for specific units of measure
+		// TODO distanceLeft/distanceRight are also instantiated here
 		
 		return (distanceRight+distanceLeft)/2;
 	}
@@ -106,9 +125,28 @@ public class SnobotPosition{
 	 */
 	public void updateAll(){
 		this.mRadianRotation=this.getSnobotDirection();
-		this.mDistanceTraveled=this.getDistanceTraveled();
+		this.mDistanceTraveled=this.getDistanceTraveled(mDefaultMeasure);
 		this.mPositionX=this.getSnobotX();
 		this.mPositionY=this.getSnobotY();
 	}
+	
+	/**
+	 * Changes the default unit of measure to
+	 * the one provided
+	 * @param aNewMeasure The desired default measure
+	 */
+	public void setNewDefaultMeasure(unitOfMeasure aNewMeasure){
+		mDefaultMeasure=aNewMeasure;
+	}
+	
+	/**
+	 * Calculates the value of mRadianRotation in degrees
+	 * @return mRadianRotation in human-friendly degrees
+	 */
+	public double getSnobotDegrees(){
+		return Math.toDegrees(mRadianRotation);
+	}
+	
+	//TODO Add logger method(s)
 
 }
