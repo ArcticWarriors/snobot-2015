e0c69726f91196478c0480ad72ddcbd2344e0c89
pjreiniger <pj.reiniger@gmail.com>
2015-01-28 12:59:14 -0500

    Updates to make these libraries java 7 compatible

--- Simulator/SimpleMockWpiLib/src/com/snobot/simulator/joysticks/KeyboardJoystick.java
+++ Simulator/SimpleMockWpiLib/src/com/snobot/simulator/joysticks/KeyboardJoystick.java
@@ -7,33 +7,33 @@ import net.java.games.input.Component.Identifier;
 public class KeyboardJoystick extends GamepadJoystick
 {
 
+	private static final Identifier[] sAXIS = new Identifier[]{
+		Identifier.Key.W,
+		Identifier.Key.A,
+		Identifier.Key.S,
+		Identifier.Key.D,
+		Identifier.Key.I,
+		Identifier.Key.J,
+		Identifier.Key.K,
+		Identifier.Key.L
+	};
+	
+	private static final Identifier[] sBUTTONS = new Identifier[]{
+		Identifier.Key._0,
+		Identifier.Key._1,
+		Identifier.Key._2,
+		Identifier.Key._3,
+		Identifier.Key._4,
+		Identifier.Key._5,
+		Identifier.Key._6,
+		Identifier.Key._7,
+		Identifier.Key._8,
+		Identifier.Key._9
+	};
+	
+
 	public KeyboardJoystick()
 	{
-		super( 
-				"Standard PS/2 Keyboard",
-
-				Arrays.asList(
-						Identifier.Key.W,
-						Identifier.Key.A,
-						Identifier.Key.S,
-						Identifier.Key.D,
-						Identifier.Key.I,
-						Identifier.Key.J,
-						Identifier.Key.K,
-						Identifier.Key.L
-				), 
-				
-				Arrays.asList(
-						Identifier.Key._0,
-						Identifier.Key._1,
-						Identifier.Key._2,
-						Identifier.Key._3,
-						Identifier.Key._4,
-						Identifier.Key._5,
-						Identifier.Key._6,
-						Identifier.Key._7,
-						Identifier.Key._8,
-						Identifier.Key._9
-				));
+		super( "Standard PS/2 Keyboard", Arrays.asList(sAXIS), Arrays.asList(sBUTTONS));
 	}
 }
