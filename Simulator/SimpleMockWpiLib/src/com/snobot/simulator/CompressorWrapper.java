f5ea27a0c4695138bcc4b35b0bed1fd89e4db020
PJ <pj.reiniger@gmail.com>
2015-02-03 23:39:31 -0500

    Adding compressor stuff

---
+++ Simulator/SimpleMockWpiLib/src/com/snobot/simulator/CompressorWrapper.java
@@ -0,0 +1,8 @@
+package com.snobot.simulator;
+
+public class CompressorWrapper 
+{
+
+	protected double mAirPressure;
+	protected double mChargeRate;
+}
--- Simulator/SimpleMockWpiLib/src/edu/wpi/first/wpilibj/hal/CompressorJNI.java
+++ Simulator/SimpleMockWpiLib/src/edu/wpi/first/wpilibj/hal/CompressorJNI.java
@@ -4,22 +4,70 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 public class CompressorJNI extends JNIWrapper {
-	public static native ByteBuffer initializeCompressor(byte module);
-	public static native boolean checkCompressorModule(byte module);
 	
-	public static native boolean getCompressor(ByteBuffer pcm_pointer, IntBuffer status);
+	public static ByteBuffer initializeCompressor(byte module)
+	{
+		return null;
+	}
+	public static boolean checkCompressorModule(byte module)
+	{
+		return true;
+	}
 	
-	public static native void setClosedLoopControl(ByteBuffer pcm_pointer, boolean value, IntBuffer status);
-	public static native boolean getClosedLoopControl(ByteBuffer pcm_pointer, IntBuffer status);
+	public static boolean getCompressor(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
 	
-	public static native boolean getPressureSwitch(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native float getCompressorCurrent(ByteBuffer pcm_pointer, IntBuffer status);
+	public static void setClosedLoopControl(ByteBuffer pcm_pointer, boolean value, IntBuffer status)
+	{
 
-	public static native boolean getCompressorCurrentTooHighFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native boolean getCompressorCurrentTooHighStickyFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native boolean getCompressorShortedStickyFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native boolean getCompressorShortedFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native boolean getCompressorNotConnectedStickyFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native boolean getCompressorNotConnectedFault(ByteBuffer pcm_pointer, IntBuffer status);
-	public static native void clearAllPCMStickyFaults(ByteBuffer pcm_pointer, IntBuffer status);
+	}
+	public static boolean getClosedLoopControl(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getPressureSwitch(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	public static float getCompressorCurrent(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return 0;
+	}
+
+	public static boolean getCompressorCurrentTooHighFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getCompressorCurrentTooHighStickyFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getCompressorShortedStickyFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getCompressorShortedFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getCompressorNotConnectedStickyFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static boolean getCompressorNotConnectedFault(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+		return false;
+	}
+	
+	public static void clearAllPCMStickyFaults(ByteBuffer pcm_pointer, IntBuffer status)
+	{
+	}
 }
