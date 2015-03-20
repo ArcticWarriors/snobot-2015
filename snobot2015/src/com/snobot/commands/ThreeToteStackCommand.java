f3c1efd88fb5c0ec4fe68f86c3fe56decbb4f2b7
PJ Reiniger <pj.reiniger@gmail.com>
2015-03-19 23:25:25 -0400

    Adding comments, updating 3 tote modes

--- snobot2015/src/com/snobot/commands/ThreeToteStackCommand.java
+++ snobot2015/src/com/snobot/commands/ThreeToteStackCommand.java
@@ -7,15 +7,28 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ThreeToteStackCommand extends Command
 {
-    IStacker mSnobotStacker;
-    Timer mTimer;
-    double mTime1;
-    double mTime2;
-    double mTime3;
-    double mTime4;
+    private IStacker mSnobotStacker;
+    private Timer mTimer;
 
-    boolean mFinishedMovingDown;
-    boolean mFinishedMovingDown2;
+    private double mTime1;
+    private double mTime2;
+    private double mTime3;
+    private double mTime4;
+
+    private enum Steps_e
+    {
+        ScoopFirst,
+        RaiseOne,
+        ScoopSecond,
+        RaiseTwo,
+        ScoopThird,
+        RaiseThree,
+        DropEm,
+
+        Finished
+    }
+
+    private Steps_e mStep;
 
     public ThreeToteStackCommand(IStacker aSnobotStacker, double aTime1, double aTime2, double aTime3, double aTime4)
     {
@@ -26,63 +39,84 @@ public class ThreeToteStackCommand extends Command
         mTime3 = aTime3;
         mTime4 = aTime4;
 
-        mFinishedMovingDown = false;
+        mStep = Steps_e.ScoopFirst;
     }
 
     @Override
     protected void initialize()
     {
-        // TODO Auto-generated method stub
         mTimer.start();
     }
 
     @Override
     protected void execute()
     {
-        // TODO Auto-generated method stub
-        if (mTimer.get() < mTime1)
+        System.out.println("Running step : " + mStep);
+
+        switch (mStep)
         {
+        // Come down on the first step
+        case ScoopFirst:
+            if (mTimer.get() > mTime1 || mSnobotStacker.moveStackerToGround())
+            {
+                mStep = Steps_e.RaiseOne;
+            }
+            break;
+
+        // Pick it up
+        case RaiseOne:
             mSnobotStacker.moveStackerToOneStack();
-            System.out.println("Phase 1");
-        }
-        else if (mTimer.get() < mTime2)
-        {
-            if (!mFinishedMovingDown)
+            if (mTimer.get() > mTime1)
             {
-
-                System.out.println("Phase 2 Down");
-                mFinishedMovingDown = !mSnobotStacker.moveStackerDown();
+                mStep = Steps_e.ScoopSecond;
             }
+            break;
 
-            if (mFinishedMovingDown)
+        // Come down on the second tote
+        case ScoopSecond:
+            if (mTimer.get() > mTime2 || mSnobotStacker.moveStackerToGround())
             {
-                System.out.println("Phase 2 Up");
-                mSnobotStacker.moveStackerToOneStack();
+                mStep = Steps_e.RaiseTwo;
             }
+            break;
 
-        }
-        else if (mTimer.get() < mTime3)
-        {
-
-            if (!mFinishedMovingDown2)
+        // Pick it up
+        case RaiseTwo:
+            mSnobotStacker.moveStackerToOneStack();
+            if (mTimer.get() > mTime2)
             {
-
-                System.out.println("Phase 3 Down");
-                mFinishedMovingDown2 = !mSnobotStacker.moveStackerDown();
+                mStep = Steps_e.ScoopThird;
             }
+            break;
 
-            if (mFinishedMovingDown2)
+        // Come down on the third tote
+        case ScoopThird:
+            if (mTimer.get() > mTime3 || mSnobotStacker.moveStackerToGround())
             {
-                System.out.println("Phase 3 Up");
-                mSnobotStacker.moveStackerToScoringPlatform();
+                mStep = Steps_e.RaiseThree;
             }
+            break;
 
-        }
-        else if (mTimer.get() < mTime4)
-        {
-            System.out.println("Phase 4");
+        // Pick it up ever so slightly
+        case RaiseThree:
+            mSnobotStacker.moveStackerToScoringPlatform();
+            if (mTimer.get() > mTime3)
+            {
+                mStep = Steps_e.DropEm;
+            }
+            break;
+
+        // Drop them and prepare to leave
+        case DropEm:
             mSnobotStacker.moveStackerToGround();
-
+            if (mTimer.get() > mTime4)
+            {
+                mStep = Steps_e.Finished;
+            }
+            break;
+        default:
+            System.out.println("WHY ARE YOU IN HERE?!?!?!?!?");
+            break;
         }
 
     }
@@ -90,22 +124,18 @@ public class ThreeToteStackCommand extends Command
     @Override
     protected boolean isFinished()
     {
-        // TODO Auto-generated method stub
-        return (mTimer.get() > mTime4);
+        return mStep == Steps_e.Finished;
     }
 
     @Override
     protected void end()
     {
-        // TODO Auto-generated method stub
-
+        mSnobotStacker.stop();
     }
 
     @Override
     protected void interrupted()
     {
-        // TODO Auto-generated method stub
-
     }
 
 }
--- snobot2015/src/com/snobot/stacker/IStacker.java
+++ snobot2015/src/com/snobot/stacker/IStacker.java
@@ -23,11 +23,25 @@ public interface IStacker extends ISubsystem
      */
     boolean moveStackerDown();
 
-    // TODO comments
+    /**
+     * Moves stacker to the ground, i.e. the lowest position possible
+     * 
+     * @return True when it is at the desired position
+     */
     boolean moveStackerToGround();
 
+    /**
+     * Moves stacker to the scoring platform height. This is the height in which we can comfortably drive onto the platform
+     * 
+     * @return True when it is at the desired position
+     */
     boolean moveStackerToScoringPlatform();
 
+    /**
+     * Moves stacker to the height where it can be comfortably above a single tote
+     * 
+     * @return True when it is at the desired position
+     */
     boolean moveStackerToOneStack();
 
 }
