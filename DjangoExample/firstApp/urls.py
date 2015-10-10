8e02e3c2037f3acb16a833b4713848efbcba0277
PJ Reiniger <pj.reiniger@gmail.com>
2015-10-09 22:01:24 -0400

    Adding a simple django example

---
+++ DjangoExample/firstApp/urls.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 9, 2015
+
+@author: PJ
+'''
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+               url(r'^$', views.index, name='index'),
+               url(r'^allData/$', views.all_data, name='allData'),
+               url(r'^person/(?P<person_id>[0-9]+)$', views.view_person, name='personPage'),
+               url(r'^pet/(?P<pet_id>[0-9]+)$', views.view_pet, name='petPage'),
+               ]
