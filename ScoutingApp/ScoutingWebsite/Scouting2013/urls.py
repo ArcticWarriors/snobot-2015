ff35669b1cbd9d8d3c789f2f6965f816d4704982
Ayush Goel <goelayush@mylcsd.org>
2015-11-19 19:52:17 -0500

    Stuff from Novemebter
    Website scouting

--- ScoutingApp/ScoutingWebsite/Scouting2013/urls.py
+++ ScoutingApp/ScoutingWebsite/Scouting2013/urls.py
@@ -9,4 +9,6 @@ from . import views
 
 urlpatterns = [
                url(r'^$', views.index, name='index'),
+               url(r'^teams$', views.all_teams, name='all_teams'),
+               url(r'^teams/(?P<team_id>[0-9]+)$', views.view_team, name='view_team'),
               ]
--- ScoutingApp/ScoutingWebsite/Scouting2013/views.py
+++ ScoutingApp/ScoutingWebsite/Scouting2013/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from Scouting2013.models import Team
 
 # Create your views here.
 
 
 def index(request):
-
+    
     return render(request, 'Scouting2013/index.html')
+
+
+def all_teams(request):
+    
+    all_team_types = Team.objects.all()
+    
+    context = {"teams": all_team_types, "black_magic": 666 }
+    
+    return render(request, 'Scouting2013/all_teams.html', context)
+
+
+def view_team(request, team_id):
+    
+    this_team = Team.objects.get(id=team_id)
+    
+    print this_team
+    
+    return render(request, 'Scouting2013/all_teams.html')
