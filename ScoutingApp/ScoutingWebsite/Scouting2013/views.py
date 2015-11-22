99357bd4414816160d3d70a20cc135b2c233bef9
PJ Reiniger <pj.reiniger@gmail.com>
2015-11-22 18:52:40 -0500

    Cleaning up some of the example stuff we did

--- ScoutingApp/ScoutingWebsite/Scouting2013/views.py
+++ ScoutingApp/ScoutingWebsite/Scouting2013/views.py
@@ -5,23 +5,23 @@ from Scouting2013.models import Team
 
 
 def index(request):
-    
+
     return render(request, 'Scouting2013/index.html')
 
 
 def all_teams(request):
-    
+
     all_team_types = Team.objects.all()
-    
-    context = {"teams": all_team_types, "black_magic": 666 }
-    
+
+    context = {"teams": all_team_types}
+
     return render(request, 'Scouting2013/all_teams.html', context)
 
 
 def view_team(request, team_id):
-    
+
     this_team = Team.objects.get(id=team_id)
-    
-    print this_team
-    
-    return render(request, 'Scouting2013/all_teams.html')
+
+    context = {"team": this_team}
+
+    return render(request, 'Scouting2013/single_team.html', context)
--- ScoutingApp/ScoutingWebsite/ScoutingWebsite/urls.py
+++ ScoutingApp/ScoutingWebsite/ScoutingWebsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^2013/', include('Scouting2013.urls')),
+    url(r'^$', include('Scouting2013.urls')),
 ]
